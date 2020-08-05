89c035ef7906b6ebc0b8b21b2c1b5b159df478d6
DanHumphry <54474732+DanHumphry@users.noreply.github.com>
2020-08-05 13:44:57 +0900

    fix

--- 생활코딩/src/component/TOC.js
+++ 생활코딩/src/component/TOC.js
@@ -6,7 +6,16 @@ class TOC extends Component{
       var data = this.props.data;
       var i = 0;
       while(i < data.length){
-        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+        lists.push(
+        <li key={data[i].id}>
+          <a
+          href={"/content/"+data[i].id}
+          // data-id={data[i].id}
+          onClick={function(id, e){
+            e.preventDefault();
+            this.props.onChangePage(id); // 주석처리한 방법 사용시 id값에  e.target.dataset.id 
+          }.bind(this, data[i].id)}
+          >{data[i].title}</a></li>);
         i = i + 1;
       }
       return (

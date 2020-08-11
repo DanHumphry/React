89e0acd4512aae35668e3cea640399cbdc286042
DanHumphry <54474732+DanHumphry@users.noreply.github.com>
2020-08-12 02:59:01 +0900

    0812 - apple

--- appleCoding/src/App.js
+++ appleCoding/src/App.js
@@ -1,36 +1,82 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  let [글제목,글제목변경] = useState(['남자 코트 추천', '남자 반팔 추천', '남자 반바지 추천']);
-  let [글내용,글내용변경] = useState(['2월 17일 발행', '6월 24일 발행', '7월 1일 발행']);
+  let [gender, ChangeGender] = useState('남자');
+  let [title, retitle] = useState([gender + '코트 추천', gender + '반팔 추천', gender + '반바지 추천']);
+  let [sub, resub] = useState(['2월 17일 발행', '6월 24일 발행', '7월 1일 발행']);
+  let [num, PlusNum] = useState([0, 0, 0]);
+  let [modal, ChangeModal] = useState(false)
 
-  function name(){
+  function ChangeGirl(){
+    let arr = [...gender];
+    arr = '여자';
+    ChangeGender(arr);
+  }
+
+  // function ChangeNumber(){
+  //   let arr2 = [...num];
+  //   arr2[0] += 1 ;
+  //   PlusNum(arr2)
+  // }
+
+    let contents = [];
     let i = 0;
-    while (i < useState.length) {
-      return (
+    let arr2 = [...num];
+    while (i < title.length) {
+      contents.push(
         <div>
-        <h3>{글제목[i]}</h3>
-        <p>{글내용[i]}</p>
+        <h3>{title[i]}<span onClick={ ()=>{
+          arr2[i] += 1 ;
+          PlusNum(arr2);
+        } }>👍</span> {num[i]} </h3>
+        <p>{sub[i]}</p>
         <hr/>
-        </div>
-      );
+      </div>
+      )
       i = i + 1;
   }
-}
 
-  let posts = "강남 고기 맛집";
+  // let posts = "강남 고기 맛집";
   return (
     <div className="App">
       <div className="black-nav">
-        <div>개발 Bolg</div>
+        <div onClick={()=>{
+          {
+            modal === false
+            ? ChangeModal(true)
+            : ChangeModal(false)
+          }
+        }}>개발 Bolg</div>
       </div>
       <div className="list">
-        {name()}
+        {contents[0]}
+        {contents[1]}
+        {contents[2]}
+        <button onClick={()=>{ChangeGirl()}}>여자</button>
+        {/* <button onClick={()=>{ChangeNumber()}}>숫자</button> */}
       </div>
+    
+    {
+      modal === true
+      ? <Modal></Modal>
+      : null
+    }
+
     </div>
   );
+}
+
+function Modal(){
+  return(
+    <div className="modal">
+    <h2>제목</h2>
+    <p>날짜</p>
+    <p>상세내용</p>
+  </div>
+  )
 }
 
 export default App;

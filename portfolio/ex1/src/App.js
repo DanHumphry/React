f66b05dedb91389d097b5649b00bb6603fd33182
DanHumphry <54474732+DanHumphry@users.noreply.github.com>
2020-11-02 03:09:48 +0900

    react portfolio

---
+++ portfolio/ex1/src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="body">
+      <div className="header">
+
+      </div>
+
+      <div className="mainBody">
+        <div className="header__margin">
+          <div className="mainBody__padding">
+          <div className="slide__back">
+            <div className="slide__back__display">
+              <div className="slide__back__section">
+                <div className="slide__back__left">
+                  
+                </div>
+                <div className="slide__back__left__center">
+                  
+                </div>
+              </div>
+
+              <div className="slide__back__section">
+                <div className="slide__back__right__center">
+    
+                </div>
+                <div className="slide__back__right">
+    
+                </div>
+              </div>
+            </div>
+            
+            </div>
+            <div className="slide__section">
+              <div className="slide__acticle__left">
+
+              </div>
+              <div className="slide__acticle__center">
+
+              </div>
+              <div className="slide__acticle__right">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
